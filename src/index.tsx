cacbdb1979b9ca2b05832adf5d778a40197330ef
Vadim Frolov <vadimf74@ya.ru>
2021-11-02 15:32:27 +0300

    error boundary finished

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import App from './App';
 
 import {store} from './store/store';
 import {Provider} from 'react-redux';
+import {ErrorBoundary} from '../src/components/ErrorBoundary/ErrorBoundary';
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <ErrorBoundary>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ErrorBoundary>
   </Provider>,
   document.getElementById('root')
 );
